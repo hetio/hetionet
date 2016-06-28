ca550fb598a1430520df1b038c12951816862f95
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2016-06-28 17:41:28 -0400

    Map log volume to host
    Former-commit-id: a802e67c73fd7b009979770281f923064ca82865

--- hetnet/neo4j/docker/ssl/renew.sh
+++ hetnet/neo4j/docker/ssl/renew.sh
@@ -7,4 +7,4 @@ cp /etc/letsencrypt/live/neo4j.het.io/fullchain.pem ~/ssl/neo4j.cert
 cp /etc/letsencrypt/live/neo4j.het.io/privkey.pem ~/ssl/neo4j.key
 
 # start docker
-docker run --detach --publish=80:7474 --publish=443:7473 --publish=7687:7687 --volume=$HOME/hetionet-data:/data --volume=$HOME/ssl:/var/lib/neo4j/certificates dhimmel/hetionet
+docker run --detach --publish=80:7474 --publish=443:7473 --publish=7687:7687 --volume=$HOME/hetionet-data:/data --volume=$HOME/neo4j-logs:/var/lib/neo4j/logs --volume=$HOME/ssl:/var/lib/neo4j/certificates dhimmel/hetionet
