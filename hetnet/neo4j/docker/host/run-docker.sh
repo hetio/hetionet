39ca502f1a27ecdf90e221861ec3ffef8da1cf27
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2016-07-15 16:56:45 -0400

    Custom Browser title & improved memory ENV

--- hetnet/neo4j/docker/host/run-docker.sh
+++ hetnet/neo4j/docker/host/run-docker.sh
@@ -1,4 +1,5 @@
 # Start dhimmel/hetionet container
+# See https://neo4j.com/developer/docker-3.x/ for doc
 docker run \
   --publish=80:7474 \
   --publish=443:7473 \
@@ -6,4 +7,6 @@ docker run \
   --volume=$HOME/hetionet-data:/data \
   --volume=$HOME/neo4j-logs:/var/lib/neo4j/logs \
   --volume=$HOME/ssl:/var/lib/neo4j/certificates \
+  --env=NEO4J_dbms_memory_pagecache_size=1G \
+  --env=NEO4J_dbms_memory_heap_maxSize=512 \
   dhimmel/hetionet
