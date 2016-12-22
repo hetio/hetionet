5f608c69352a758a2eac506eb72cb1c78007ce0c
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2016-12-22 18:14:10 -0500

    Update guides, version db download url
    Former-commit-id: 319673109a5f7948dd5618843bf0015e8dc776ff

--- hetnet/neo4j/docker/scripts/get_data.sh
+++ hetnet/neo4j/docker/scripts/get_data.sh
@@ -8,7 +8,7 @@ cd /data
 mkdir -p databases
 if [ ! -d "databases/graph.db" ]; then
   echo "Downloading and extracting database"
-  DB_URL="https://github.com/dhimmel/hetionet/raw/neo4j-3.0/hetnet/neo4j/hetionet-v1.0.db.tar.bz2"
+  DB_URL="https://github.com/dhimmel/hetionet/raw/e319b1b50639b845a35e31fbc16afcbe9b8c2dfd/hetnet/neo4j/hetionet-v1.0.db.tar.bz2"
   curl --silent --location $DB_URL | tar --extract --bzip2 --directory=databases
 else
   echo "Not retrieving database as it already exists"
@@ -17,7 +17,7 @@ fi
 if [ ! -d "guides" ]; then
   echo "Downloading and extracting guides"
   cp --recursive /guides ./
-  GUIDES_URL="https://github.com/dhimmel/het.io-rep-guides/raw/fe74cd6db34cddc379f789e5ce5fe6b2c32abf2d/guides.tar.bz2"
+  GUIDES_URL="https://github.com/dhimmel/het.io-rep-guides/raw/4837f3dcdcdb56b14fdc2b998c8571ae7aad6b36/guides.tar.bz2"
   mkdir guides/rep
   curl --silent --location $GUIDES_URL | tar --extract --bzip2 --strip-components=1 --directory=guides/rep
 else
