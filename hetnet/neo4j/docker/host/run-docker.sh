3e8379c475623525eff1dd08b7576212351b7707
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-01-04 14:10:50 -0500

    Update configuration for Neo4j 3.1
    Closes https://github.com/dhimmel/hetionet/issues/2.
    
    
    Former-commit-id: f92e54c2bf1230f330d9933166fafa5e7bc3a9e7

--- hetnet/neo4j/docker/host/run-docker.sh
+++ hetnet/neo4j/docker/host/run-docker.sh
@@ -8,6 +8,6 @@ docker run \
   --volume=$HOME/hetionet-data:/data \
   --volume=$HOME/neo4j-logs:/var/lib/neo4j/logs \
   --volume=$HOME/ssl:/var/lib/neo4j/certificates \
-  --env=NEO4J_dbms_memory_pagecache_size=1G \
-  --env=NEO4J_dbms_memory_heap_maxSize=512 \
+  --env=NEO4J_dbms_memory_pagecache_size=1g \
+  --env=NEO4J_dbms_memory_heap_maxSize=512m \
   dhimmel/hetionet
