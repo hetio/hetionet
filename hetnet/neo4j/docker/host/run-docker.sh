c4453e1a3d95ab30d04b2f9b979a2e23ffb6101a
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-06-22 11:51:10 -0400

    Tweak Neo4j config to fix OutOfMemoryError
    Attempts to fix:
    
    java.lang.OutOfMemoryError: GC overhead limit exceeded
    
    Refs https://github.com/neo4j/neo4j/issues/9209
    
    From https://neo4j.com/docs/operations-manual/3.2/performance/#page-cache-sizing
    
    You can determine the total memory needed for the page cache by summing up the sizes of
    the NEO4J_HOME/data/databases/graph.db/*store.db* files and adding 20% for growth.
    
    Hence, seems that 512m is sufficient for page cache.
    
    
    Former-commit-id: 3f8dd90941384e94fef428dce0cc1c0d6d0c2bc7

--- hetnet/neo4j/docker/host/run-docker.sh
+++ hetnet/neo4j/docker/host/run-docker.sh
@@ -3,13 +3,13 @@
 docker run \
   --detach \
   --name=hetionet-container \
-  --restart=on-failure:50 \
+  --restart=on-failure \
   --publish=80:7474 \
   --publish=443:7473 \
   --publish=7687:7687 \
   --volume=$HOME/hetionet-data:/data \
   --volume=$HOME/neo4j-logs:/logs \
   --volume=$HOME/ssl:/ssl \
-  --env=NEO4J_dbms_memory_pagecache_size=1g \
-  --env=NEO4J_dbms_memory_heap_maxSize=512m \
+  --env=NEO4J_dbms_memory_pagecache_size=512m \
+  --env=NEO4J_dbms_memory_heap_maxSize=1g \
   dhimmel/hetionet
